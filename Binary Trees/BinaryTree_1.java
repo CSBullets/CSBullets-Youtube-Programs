fe924354c26847404f4d7fe54a9755f51f150b82
ARJUN-SABU <arjunsabu99@gmail.com>
2022-02-09 21:45:47 +0530

    Added code for Binary Tree + display() method

--- Binary Trees/BinaryTree_1.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class BinaryTree_1{
-    public static void main(String [] args){
-        Scanner s = new Scanner(System.in);
-        System.out.println("Hello");
-    }
-}
